d636a215c39f5450b6ae3177ba87919683a9dec9
ginsama <jefferytutorials@gmail.com>
2016-02-22 15:54:30 +0000

    Lib phantomjs function, wrapSync call server side

--- server/methods.js
+++ server/methods.js
@@ -88,22 +88,12 @@ Meteor.methods({
 	},
 	scrapeThisUrlByXpath: function(url, xpath, id = null){
 		try{
-	        command = UrlWatcher.spawn(UrlWatcher.phantomjs.path, ['assets/app/phantomDriver.js', url, xpath]);
-	        var response = false;
 
-	        command.stdout.on('data', function (data) {
-	            response = data;
-	        });
-	        command.stderr.on('data', function (data) {
-	            console.log('stderr: ' + data);
-	            response = data;
-	        });
-	        command.on('exit', function (code) {
-	            process.exit(code);
-	        });
+	        return Meteor.wrapAsync(
+	        	Meteor.functions.phantomjs(xpath, url)
+	        );
 
-	        if(response)
-	        	return response;
+
 		}catch(e){
 			return e;
 		}
